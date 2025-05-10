3b985c80c99d0f5c492d098533a322db0b047bdd
voquangthan <voquangthan1234@gmail.com>
2025-05-10 13:50:04 +0700

    3

---
+++ app/src/main/java/com/example/project_bangcuuchuong/HocActivity1.java
@@ -0,0 +1,26 @@
+package com.example.project_bangcuuchuong;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class HocActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hoc1);
+        FragmentManager fragment=getSupportFragmentManager();
+        fragment.beginTransaction()
+                .add(R.id.frameheader1,new HeaderFragment())
+                .add(R.id.framefooter1,new FooterFragment())
+                .commit();
+
+    }
+}
--- app/src/main/java/com/example/project_bangcuuchuong/MainActivity.java
+++ app/src/main/java/com/example/project_bangcuuchuong/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.project_bangcuuchuong;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button_hoc;
+Button button_choi;
+Button button_danhgia;
+Button button_thidau;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        button_hoc=findViewById(R.id.btnmain_hoc);
+        button_choi=findViewById(R.id.btnmain_choi);
+        button_danhgia=findViewById(R.id.btnmain_danhgia);
+        button_thidau=findViewById(R.id.btnmain_thidau);
+        button_hoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,HocActivity1.class);
+                startActivity(intent);
+
+            }
         });
     }
 }
